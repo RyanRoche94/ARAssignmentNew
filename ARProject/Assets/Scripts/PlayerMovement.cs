083e0955ac0e50b9e44e7bbb6d365679e155d83d
Naomi Kaufman <accounts@warducks.com>
2021-11-23 20:50:38 +0000

    Camera Prefab

--- ARProject/Assets/Scripts/PlayerMovement.cs
+++ ARProject/Assets/Scripts/PlayerMovement.cs
@@ -1,17 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public CharacterController controller;
+    public float speed = 6f;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
         
     }
