d9ad6fc687c2024e1564c229f8dd8daf98347669
Naomi Kaufman <accounts@warducks.com>
2021-12-14 11:11:16 +0000

    Levels 1 and 2

---
+++ ARProject/Assets/Scripts/SceneChange.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+
+    int nextSceneIndex;
+
+    public void Awake()
+    {
+       nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+    }
+    
+   
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (SceneManager.sceneCount > nextSceneIndex)
+            {
+                SceneManager.LoadScene(nextSceneIndex);
+            }
+        }
+    }
+}
